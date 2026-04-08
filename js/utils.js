ebf4e84bd930ca43b9ddc93d83e0ddd186d788c1
Mikita Buinavets <nikita.492@yandex.by>
2026-04-08 13:53:45 +0300

    Fix paging

--- js/utils.js
+++ js/utils.js
@@ -24,14 +24,13 @@ function highlightRows(tbody) {
         row.addEventListener('animationend', () =>
             row.classList.remove('row-highlight'), {
             once: true
-        }
-        );
+        });
     });
 }
 
 function updatePagination(state, pageInfoSpan, prevBtn, nextBtn, ITEMS_PER_PAGE) {
     const totalPages = Math.ceil(state.filteredPosts.length / ITEMS_PER_PAGE) || 1;
-    pageInfoSpan.textContent = `${state.currentPage} of ${totalPages} pages`;
-    prevBtn.disabled = state.currentPage <= 1;
-    nextBtn.disabled = state.currentPage >= totalPages;
+    pageInfoSpan.textContent = `${state.curPage} of ${totalPages} pages`;
+    prevBtn.disabled = state.curPage <= 1;
+    nextBtn.disabled = state.curPage >= totalPages;
 }
